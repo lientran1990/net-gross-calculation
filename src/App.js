240f23d0a476718174522697fa4946513115539b
Lien Tran <tranthilien.m@gmail.com>
2023-11-01 15:46:06 +0700

    feat(calculate): gross to net for V1

--- src/App.js
+++ src/App.js
@@ -3,13 +3,30 @@ import * as React from "react";
 import Button from "@mui/material/Button";
 import TextField from "@mui/material/TextField";
 import { width } from "@mui/system";
+import { useState } from "react";
+
+function luongdongbaohiem(grossSalary) {
+  var luongcoso = 1800000;
+  var luongtoithieuvung = 4680000;
+  const gtgc = 11000000;
+  if (grossSalary >= luongtoithieuvung && grossSalary <= 20 * luongcoso) {
+    return grossSalary;
+  } else if (grossSalary < luongtoithieuvung) {
+    return luongtoithieuvung;
+  } else {
+    return luongcoso * 20;
+  }
+}
 
 function App() {
-  // const inputRef = useRef(null);
-  // const resultRef = useRef(null);
-  // const [result, setResult] = useState(0);
+  const [grossSalary, setGrossSalary] = useState("");
+  const [result, setResult] = useState("");
 
-  const handleClick = (event) => {};
+  const handleClick = (event) => {
+    var tienbaohiem = (luongdongbaohiem(grossSalary) * 10.5) / 100;
+
+    setResult(grossSalary - tienbaohiem);
+  };
 
   return (
     <div className="App">
@@ -17,12 +34,13 @@ function App() {
       <p>Apply from 01/07/2023 (Latest) </p>
       <TextField
         style={{ margin: 1 + "rem" }}
-        id="standard-basic"
+        id="gross-salary"
+        value={grossSalary}
+        onChange={(e) => setGrossSalary(e.target.value)}
         type="number"
-        label="Income"
+        label="Gross Salary"
         variant="standard"
       />
-
       <br />
       <TextField
         style={{ margin: 1 + "rem" }}
@@ -30,11 +48,11 @@ function App() {
         label="Number of dependents"
         variant="standard"
       />
-      <div>
-        <Button variant="contained" onClick={handleClick}>
-          Gross to Net
-        </Button>
-      </div>
+      <br />
+      <Button variant="contained" onClick={handleClick}>
+        Gross to Net
+      </Button>
+      <p>Result: {result}</p>
     </div>
   );
 }
